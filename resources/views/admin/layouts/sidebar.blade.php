f01e23d98ada8c01b82386c5e183af9f9761b253
najeebulhassan <35789426+najeebulhassan@users.noreply.github.com>
2020-10-19 19:59:10 +0500

    update sidebar

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -27,7 +27,7 @@
                     <div id="mainnav-profile" class="mainnav-profile">
                         <div class="profile-wrap text-center">
                             <div class="pad-btm">
-                                <img class="img-circle img-md" src="img/profile-photos/1.png" alt="Profile Picture">
+                                <img class="img-circle img-md" src="public/assets/img/profile-photos/1.png" alt="Profile Picture">
                             </div>
                             <a href="#profile-nav" class="box-block" data-toggle="collapse" aria-expanded="false">
                                 <span class="pull-right dropdown-toggle">
