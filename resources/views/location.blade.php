d182bf9d8dacdfe5162ad52393f59eec5ef2eed4
najeebulhassan <35789426+najeebulhassan@users.noreply.github.com>
2020-11-12 17:23:02 +0500

    new pages

---
+++ resources/views/location.blade.php
@@ -0,0 +1,101 @@
+@extends('layouts.base')
+@section('title','Location')
+@section('content')
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+
+<div class="jp_tittle_main_wrapper">
+    <div class="jp_tittle_img_overlay"></div>
+    <div class="container">
+        <div class="row">
+
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+                <div class="jp_tittle_heading_wrapper">
+
+                    <div class="jp_tittle_heading">
+
+                        <h2>Location</h2>
+
+                    </div>
+
+                    <div class="jp_tittle_breadcrumb_main_wrapper">
+
+                        <div class="jp_tittle_breadcrumb_wrapper">
+
+                            <ul>
+
+                                <li><a href="{{url('/')}}">Home</a> <i class="fa fa-angle-right"></i></li>
+
+
+
+                                <li>Location</li>
+
+                            </ul>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+<div class="aboutus_section">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-lg-12 col-md-12 col-xs-12 col-sm-12 pl-0">
+
+                <div class="about_text_wrapper">
+
+                    <div class="section_heading section_2_heading">
+
+                        <h1>Locations</span></h1>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class="col-lg-12 col-md-12 col-xs-12 col-sm-12 pl-0" >
+
+                <div class="about_text_wrapper row margin-top-60">
+
+                    <div class="col-lg-4 col-md-4 col-xs-12 col-sm-12" style="text-align: center;">
+                        <img class="image-style-border" src="{{url('public/assets/images/page-pictures/EVENT STAFF/event_staff_2.jpg')}}" width="100%" height="250px" alt="Event Staff">
+                        <h2 class="margin-top-20">RECRUITMENT AGENCY ILFORD, GREATER LONDON</h2>
+                        <p>Ilford, Greater London 344-348 High Road, IG1 1QP</p>
+                        <h3><strong> Tel: +442030869080 </strong></h3>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-xs-12 col-sm-12 " style="text-align: center;">
+                        <img class="image-style-border" src="{{url('public/assets/images/page-pictures/london-recruitment-agencies/london-recruitment-agencies-3.jpg')}}" width="100%" height="250px" alt="Event Staff">
+                        <h2 class="margin-top-20">RECRUITMENT AGENCY CENTRAL LONDON</h2>
+                        <p>207 Regent Street, Marylebone, London W1A 6US</p>
+                        <h3><strong> Tel: +443332422711 </strong></h3>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-xs-12 col-sm-12" style="text-align: center;">
+                        <img class="image-style-border" src="{{url('public/assets/images/page-pictures/london-recruitment-agencies/london-recruitment-agencies-2.jpg')}}" width="100%" height="250px" alt="Event Staff">
+                        <h2 class="margin-top-20">RECRUITMENT AGENCY MANCHESTER</h2>
+                        <p>Unit 20648, Oldham Road, Manchester. M61 0BW</p>
+                        <h3><strong> Tel: +443332422711 </strong></h3>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+</div>
+
+
+
+@endsection
