7d3416bafc49d8ecaec1d0896e65c2e643ef6a8a
najeebulhassan <35789426+najeebulhassan@users.noreply.github.com>
2020-11-27 21:19:26 +0500

    best-temping-london update

--- resources/views/employer.blade.php
+++ resources/views/employer.blade.php
@@ -893,69 +893,8 @@
 
 <!-- aboutus_section end -->
 
-<div class="aboutus_page_2_section">
+@include('layouts.how-we-work')
 
-    <div class="container">
-
-        <div class="row">
-
-            <div class="col-lg-12 col-md-12 col-xs-12 col-sm-12">
-
-                <div class="aboutus_text_section abt_txt_page_2">
-
-                    <h2>How We <span>Work</span></h2>
-
-                    <p>We do not depend always on technology for candidate searches, preferring to use experience, industry acumen and intuition to identify what each individual could bring to a specific business. This intelligent & human approach pays surpluses time after time. We recognise the trust clients make in Workers Direct and our priority is always to make sure we attain the best results for all concerned.</p>
-
-                    <div class="row">
-
-                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 row margin-top-40">
-                            <div class="margin-top-20 col-md-3 col-sm-12">
-                                <div class="margin-top-10 jp_best_deal_icon_sec"> <i class="green fa fa-user"></i> </div>
-                                <div class="jp_best_deal_cont_sec margin-top-15">
-                                    <h4><a href="{{url('register')}}">1. Register </a> <span style="color: #7CC24D;">/</span> <a href="{{url('login')}}"> Login</a></h4>
-                                    <p> Our professional recruiter will contact you within no time to find out your hiring needs, skills and experience of an ideal candidate & finally the job duration and location.</p>
-                                </div>
-                            </div>
-                            <div class="margin-top-20 col-md-3 col-sm-12">
-                                <div class="margin-top-10 jp_best_deal_icon_sec"> <i class="green fa fa-address-card"></i> </div>
-                                <div class="jp_best_deal_cont_sec margin-top-15">
-                                    <h4><a href="{{url('submit-a-job')}}">2. Post A Job</a></h4>
-                                    <p> If you would like to hire any staff either is temporary or permanent basis, Post A Job on our website or via mobile app. </p>
-                                </div>
-                            </div>
-                            <div class="margin-top-20 col-md-3 col-sm-12">
-                                <div class="margin-top-10 jp_best_deal_icon_sec"> <i class="fas fa-search"></i> </div>
-                                <div class="jp_best_deal_cont_sec margin-top-15">
-                                    <h4>3. We will find the right match</h4>
-                                    <p> If you want to hire you can Submit a Job. We have a large pool of candidates, who always ready to start a job on short notice.</p>
-                                </div>
-                            </div>
-                            <div class="margin-top-20 col-md-3 col-sm-12">
-                                <div class="margin-top-10 jp_best_deal_icon_sec"> <i class="green fa fa-get-pocket"></i> </div>
-                                <div class="jp_best_deal_cont_sec margin-top-15">
-                                    <h4>4. Job Done</h4>
-                                    <p> Upon your confirmation we will book the candidate to start the job.</p>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-
-
-
-                </div>
-
-            </div>
-
-
-            <!--end of /.col-sm-6-->
-
-        </div>
-
-    </div>
-
-</div>
 
 <!--end of /.col-sm-6-->
 
